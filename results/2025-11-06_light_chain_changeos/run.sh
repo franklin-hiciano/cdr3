519616b68a654418c874d17242fda865bb6fe21d
Franklin Hiciano <fhiciano5@gmail.com>
2025-11-06 12:39:17 -0500

    added file splitting

--- results/2025-11-06_light_chain_changeos/run.sh
+++ results/2025-11-06_light_chain_changeos/run.sh
@@ -7,4 +7,12 @@ function download_changeo {
 	# from https://drive.google.com/file/d/1acJkm2RtxALwEQHxUwP6inbXMQ1vOoLr/view?usp=drive_link
 }
 
+function split_file {
+	for chain in IGK IGL
+do
+	awk -v -s=${chain} '{if ($?? == s) print $0}' > ${chain}.tsv
+done
+}
 
+
+split_file
