b557221093aa3a9752318e86e95a6ec9814a9c48
Franklin Hiciano <fhiciano0@gmail.com>
2026-01-12 19:46:21 -0500

    Changed indenting

--- results/2025-11-06_light_chain_changeos/R/run.R
+++ results/2025-11-06_light_chain_changeos/R/run.R
@@ -7,15 +7,15 @@ plot_clones < function() {
   changeo_IGL <- read.csv(paste0(CHANGEO_DIR, "IGL", "/", "master_changeo_unmutated_500_seqs_filtered_productive.tsv"), sep='\t')
 
   for (changeo in list(changeo_IGK, changeo_IGL)) {
-  clone_counts <- changeo %>% group_by(.data[["sample_id"]]) %>% summarize(unique_clones = n_distinct(clone_id))
-  p <- ggplot(clone_counts, aes(x="", y=unique_clones)) +
-    geom_boxplot(outlier.shape=NA) + 
-    labs(title = paste0("Clone counts for ", changeo$locus[[1]], "")) +
-    geom_jitter(width=0.1, size = 2) +
-    theme_minimal() +
-    labs(x="Samples", y="Number of unique clones") +
-  print(p)
-}
+    clone_counts <- changeo %>% group_by(.data[["sample_id"]]) %>% summarize(unique_clones = n_distinct(clone_id))
+    p <- ggplot(clone_counts, aes(x="", y=unique_clones)) +
+      geom_boxplot(outlier.shape=NA) + 
+      labs(title = paste0("Clone counts for ", changeo$locus[[1]], "")) +
+      geom_jitter(width=0.1, size = 2) +
+      theme_minimal() +
+      labs(x="Samples", y="Number of unique clones") +
+    print(p)
+  }
 }
 
 
