baa2c0d28a3befaa420f8f10f7ed8a30ae6121bb
Franklin Hiciano <fhiciano5@gmail.com>
2026-01-12 19:41:55 -0500

    made r plots

--- results/2025-11-06_light_chain_changeos/R/run.R
+++ results/2025-11-06_light_chain_changeos/R/run.R
@@ -1,6 +1,31 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(tidyverse)
+
+CHANGEO_DIR <- ("/sc/arion/scratch/hiciaf01/projects/cdr3/data/2025-11-06_light_chain_changeos/changeo/")
+
+list.files(paste0(CHANGEO_DIR, "IGK"))
+
+changeo_IGK <- read.csv(paste0(CHANGEO_DIR, "IGK", "/", "master_changeo_unmutated_500_seqs_filtered_productive.tsv"), sep='\t')
+changeo_IGL <- read.csv(paste0(CHANGEO_DIR, "IGL", "/", "master_changeo_unmutated_500_seqs_filtered_productive.tsv"), sep='\t')
+
+
+
+for (changeo in c(changeo_IGK, changeo_IGL)) {
+  clone_counts <- changeo %>% group_by(sample_id) %>% summarize(unique_clones = n_distinct(sequence_aa))
+  ggplot(clone_counts, aes(x="", y=unique_clones)) +
+    geom_boxplot()
+}
+
+unique(changeo$sequence_aa)  
+
+
+boxplot()
+
+
+
+#old code
 
 base_dir <- "/sc/arion/work/hiciaf01/projects/cdr3/results/2025-11-06_light_chain_changeos"
 
